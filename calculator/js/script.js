d12485421203943b11b4ea557b9d99b9795a7fed
DogsOfTheWorld <86607319+DogsOfTheWorld@users.noreply.github.com>
2021-06-30 12:57:37 -0400

    Create our ToDo-List

---
+++ calculator/js/script.js
@@ -0,0 +1,26 @@
+const input1 = document.querySelector('#input1')
+const input1 = document.querySelector('#input1')
+
+const additionInput = document.querySelector('#addition')
+const subtractionInput = document.querySelector('#subtraction')
+const multiplicationInput = document.querySelector('#multiplication')
+const divisionInput = document.querySelector('#division')
+
+const output = document.querySelector('#output')
+
+additionInput.addEventListener('click', e => {
+  alert("clicked")
+  output.innerHTML = input1.value + input2.value
+})
+
+subtractionInput.addEventListener('click', e =>{
+
+})
+
+multiplicationInput.addEventListener('click', e =>{
+
+})
+
+divisionInput.addEventListener('click', e =>{
+
+})
---
+++ todo-list/js/script.js
@@ -0,0 +1,36 @@
+const input_bar = document.querySelector('#input_bar');
+const submit_button = document.querySelector('#submit_button');
+
+const color_toggle = document.querySelector('#color_toggle')
+
+submit_button.addEventListener('click', e => {
+
+if(input_bar.value == '') {
+  return;
+}
+
+const entry = document.createElement('div');
+entry.classList.add('entry')
+
+const entry_text = document.createElement('p');
+entry_text.classList.add('entry_text')
+entry_text.innerHTML = input_bar.value
+
+entry.appendChild(entry_text)
+
+//Delete the div when we click on it
+
+entry.addEventListener('click', e => {
+  e.target.remove();
+})
+
+document.querySelector('#events').appendChild(entry)
+
+input_bar.value = ''
+
+})
+
+color_toggle.addEventListener('click', e => {
+  document.querySelector('body').classList.toggle('dark');
+
+})
